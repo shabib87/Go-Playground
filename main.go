236f374c5811affa498210cdb480214cd7e4fad8
Ahmad Shabibul Hossain <ahmad.hossain@vts.com>
2023-03-04 15:06:57 -0500

    cleaned up code for server start and interuption

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"Go-Playground/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -16,9 +19,34 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	log.Println("Starting Server")
-	error := http.ListenAndServe(":9090", sm)
-	if error != nil {
-		log.Fatal(error)
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
+
+	go func() {
+		l.Println("Starting Server")
+		err := s.ListenAndServe()
+
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	l.Println("Got signal: ", sig)
+
+	ctx, err := context.WithTimeout(context.Background(), 30*time.Second)
+	if err != nil {
+		l.Println("Error: ", err)
+	}
+	s.Shutdown(ctx)
 }
