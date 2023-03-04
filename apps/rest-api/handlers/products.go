c13d62b0afa15c2b9a8b5b19465c3a7a2e6eb6ec
Ahmad Shabibul Hossain <ahmad.hossain@vts.com>
2023-03-04 17:45:18 -0500

    added rest api with get

---
+++ apps/rest-api/handlers/products.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"Go-Playground/apps/rest-api/data"
+	"log"
+	"net/http"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		p.getProducts(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+
+	if err != nil {
+		http.Error(rw, "Unable to parse", http.StatusInternalServerError)
+	}
+}
+
